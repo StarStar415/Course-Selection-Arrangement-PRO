b856327162697a241423cee72ba068e498831b97
StarStar <saba0516820@gmail.com>
2024-01-01 21:41:52 +0800

    feat: delete user_select_class

---
+++ delete_course.php
@@ -0,0 +1,26 @@
+<?php
+
+$user = 'root';
+$password = '01057132';
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=final_project;charset=utf8', $user, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $Course_ID = $_POST['Course_ID'];
+    $User_Name = $_POST['User_Name'];
+    $Grade = $_POST['Grade'];
+
+    // 使用 DELETE 查詢來刪除資料
+    $query = "DELETE FROM user_class WHERE Course_ID = ? AND User_Name = ? AND Grade =?";
+    $stmt = $db->prepare($query);
+
+    $stmt->execute(array($Course_ID, $User_Name, $Grade));
+
+    $db = null;
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+?>
--- select_script.js
+++ select_script.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
     // 將事件監聽器附加到 checkbox 上
     $('#selectionClass').on('change', 'input[type="checkbox"]', function () {
-      if ($(this).is(':checked')) {
+        
         var currentRow = $(this).closest('tr');
-        console.log(currentRow);
-  
         var Course_ID = currentRow.find('td:eq(1)').text(); 
         var Course_Name = currentRow.find('td:eq(2)').text(); 
         var Dept_Name = currentRow.find('td:eq(3)').text();
@@ -14,40 +12,50 @@ $(document).ready(function () {
         var Class_Type = currentRow.find('td:eq(7)').text();
         var Time = currentRow.find('td:eq(8)').text();
         var User_Name = $('#user_name').text();
+        if ($(this).is(':checked')) {
+            console.log("insert");
+            console.log(currentRow);
 
-  
-        console.log('課程 ID：' + Course_ID);
-        console.log('課程名稱：' + Course_Name);
-        console.log('系所：' + Dept_Name);
-        console.log('年級：' + Grade);
-        console.log('老師：' + Teacher_Name);
-        console.log('學分：' + Credit);
-        console.log('課程類型：' + Class_Type);
-        console.log('時間：' + Time);
-        console.log('使用者：' + User_Name);
-  
-        
-        $.ajax({
-          type: 'POST',
-          url: 'insert_course.php', 
-          data: {
-            Course_ID: Course_ID,
-            Course_Name: Course_Name,
-            Dept_Name: Dept_Name,
-            Grade: Grade,
-            Teacher_Name: Teacher_Name,
-            Credit: Credit,
-            Class_Type: Class_Type,
-            Time: Time,
-            User_Name: User_Name
-          },
-          success: function (response) {
-            console.log(response);
-          },
-          error: function (error) {
-            console.error('錯誤：', error);
-          }
-        });
-      }
+            $.ajax({
+            type: 'POST',
+            url: 'insert_course.php', 
+            data: {
+                Course_ID: Course_ID,
+                Course_Name: Course_Name,
+                Dept_Name: Dept_Name,
+                Grade: Grade,
+                Teacher_Name: Teacher_Name,
+                Credit: Credit,
+                Class_Type: Class_Type,
+                Time: Time,
+                User_Name: User_Name
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.error('錯誤：', error);
+            }
+            });
+        }
+        else{
+            console.log("delete");
+            console.log(currentRow);
+            $.ajax({
+            type: 'POST',
+            url: 'delete_course.php', 
+            data: {
+                Course_ID: Course_ID,
+                Grade: Grade,
+                User_Name: User_Name
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.error('錯誤：', error);
+            }
+            });
+        }
     });
   });
